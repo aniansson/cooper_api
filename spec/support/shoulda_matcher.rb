04e798e4dffa975dd39c33c2b3f244ac883a479d
aniansson <ebba@aniansson.se>
2017-01-18 20:34:27 +0100

    Make requests

--- spec/support/shoulda_matcher.rb
+++ spec/support/shoulda_matcher.rb
@@ -1,10 +1,10 @@
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
-    with.test_frameork :rspec
+    with.test_framework :rspec
     with.library :rails
   end
 end
 
 RSpec.configure do |config|
-  config.included(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
